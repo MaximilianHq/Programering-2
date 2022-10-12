3801bf99c4649cac88f2a14be01d4606cd3c1455
Maximilian Hallqvist <98323670+NoLifer47@users.noreply.github.com>
2022-10-12 21:14:08 +0200

    switch

--- Das game/game_objects.py
+++ Das game/game_objects.py
@@ -1,24 +1,69 @@
-import pygame
+import pygame, random
 
 class Player:
+    
+    #variables
     def __init__(self, x, y):
         self.x=x
         self.y=y
-        self.size=20
-        self.evospd=1
-        self.xspd=10
-        self.yspd=10
+        self.size=19
+        self.evospd=0.1
+        self.yspd=1
+        self.xspd=2
         self.width=400
         self.height=250
         
+        self.r=0
+        self.g=0
+        self.b=0
         
+        self.crash_sound = pygame.mixer.Sound("boop.wav")
+        
+    #player transformation
     def update(self):
-        self.size+=self.speed
-        if(self.size>=15 or self.size<=0):
-            self.speed=-self.speed
+        self.size += self.evospd
+        if(self.size >= 20 or self.size <= 17.5):
+            self.evospd = -self.evospd
+            
+    #move function
+    def move(self, dir):
+        if dir=="x":
+            self.x += self.xspd
+        if dir=="y":
+            self.y += self.yspd
+            
+    #set transformation coordinates / limits
+    def flipDir(self, dir):
+        if(dir=="x"):
+            self.xspd = -self.xspd
+        elif(dir=="y"):
+            self.yspd = -self.yspd
+        
+        pygame.mixer.Sound.play(self.crash_sound)
+        
+    #light up border when hit
+    def border_hit(self, screen):
+        rgbr=self.r, self.g, self.b
+        #y top
+        if self.y < 0+self.size:
+            pygame.draw.line(screen, (rgbr), [0, 0], [self.width, 0], 10)
+        #x right
+        elif self.x > self.width-self.size:
+            pygame.draw.line(screen, (rgbr), [self.width, 0], [self.width, self.height], 10)
+        #y bottom 
+        elif self.y > self.height-self.size:
+            pygame.draw.line(screen, (rgbr), [self.width, self.height], [0, self.height], 10)
+        #x left 
+        elif self.x < 0+self.size:
+            pygame.draw.line(screen, (rgbr), [0, self.height], [0, 0], 10) 
+         
+    #randomize rgb values   
+    def color_shift(self, n):
+        if n=="true":
+            self.r=random.randint(0,255)
+            self.g=random.randint(0,255)
+            self.b=random.randint(0,255)
  
+    #player attributes
     def draw(self, screen):
-        pygame.draw.circle(screen,(255, 0, 0),(self.x, self.y), self.size)
-        
-    def flip_dir(self, ):
-        
+        pygame.draw.circle(screen,(self.r, self.g, self.b),(self.x, self.y), self.size)
--- Das game/test/app.py
+++ Das game/test/app.py
@@ -1,44 +1,39 @@
-import pygame, sys, game_objects, random
+import pygame, sys
 from pygame.locals import QUIT
 
-pygame.init()
+import game_objects
 
+pygame.init()
 #screen size
 DISPLAYSURF = pygame.display.set_mode((400,250))
 pygame.display.set_caption('Das Game')
 
+
 #initiations
 clock=pygame.time.Clock()
-player=game_objects.Player(0,0)
-#set random spawn
-player.x=random.randint(player.size, player.width)
-player.y=random.randint(player.size, player.height)
-
+player=game_objects.Player(200, 150)
 
 while True:
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
-            
+    #clear window
     pygame.display.update()
-    clock.tick(60)
-    player.update()
+    #update
+    clock.tick(15)
+    #player.update()
     DISPLAYSURF.fill((255, 255, 255))
+    #display
     player.draw(DISPLAYSURF)
-    
-    player.move("x")
-    player.move("y")
-    
-    #when player hits border
+
+    player.move(player.x, player.xSpeed)
+    player.move(player.y, player.ySpeed)
+
     if(player.x > player.width-player.size or player.x < 0+player.size):
         player.flipDir("x")
-        player.color_shift("true")
-        player.border_hit(DISPLAYSURF)
-    #when player hits border
+        player.randomColor()
+    
     if(player.y > player.height-player.size or player.y < 0+player.size):
         player.flipDir("y")
-        player.color_shift("true")
-        player.border_hit(DISPLAYSURF)
-    
-    print("x:", player.x, "  y:", player.y)
+        player.randomColor()
